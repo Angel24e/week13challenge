acd2f93a2aa0ec1990810458904a6f78e7d44ab7
angel Escamilla <angel4esc@gmail.com>
2023-03-08 18:34:24 -0700

    done

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -6,7 +6,7 @@ const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
 Product.belongsTo(Category, {
-  foreignKey: 'product_id'
+  foreignKey: 'category_id'
 });
 
 // Categories have many Products
@@ -16,22 +16,14 @@ Category.hasMany(Product, {
 
 // Products belongToMany Tags (through ProductTag)
 Product.belongsToMany(Tag, {
-  through: {
-    model: ProductTag,
-    unique: false
-  },
-  
-  as: 'product_tags'
+  through: ProductTag,
+  foreignKey: 'product_id',
 });
 
 // Tags belongToMany Products (through ProductTag)
 Tag.belongsToMany(Product, {
-  through: {
-    model: ProductTag,
-    unique: false
-  },
-  
-  as: 'tags_product'
+  through: ProductTag,
+  foreignKey: 'tag_id',
 });
 
 
